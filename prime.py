3deea019fe98adaac36dab07af4f62492c1311cf
imran78633 <40804607+imran78633@users.noreply.github.com>
2018-07-11 14:27:12 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,10 @@
+def prime(num):
+    if num > 1:
+        for i in range(2,num):
+            if (num % i) == 0:
+            	return "no"
+            else:
+            	return "yes"
+    else:
+    	return "no"
+print(prime(int(input())))
